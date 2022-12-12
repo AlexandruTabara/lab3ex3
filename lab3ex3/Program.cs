548de7c99b2c0a6a72f7c0f23f64d006caa5cdb0
Alexandru Tabara <alexandru.tab1@gmail.com>
2022-12-12 13:46:57 +0200

    Add project files.

---
+++ lab3ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab3ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 3
+            //Scrieti o functie care va determina daca un numar este sau nu patrat perfect. Apelati - o si afisati-i rezultatu
+
+            bool testPatratPerfect = PatratPerfect(81);
+            Console.WriteLine(testPatratPerfect);
+
+            static bool PatratPerfect(int number)
+            {
+                return (Math.Sqrt(number) % 1 == 0);
+            }
+        }
+    }
+}
